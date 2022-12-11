8f3729dfe94609e1a954c38bd157c01672fc2e8d
Raaed Shaikh <raaed@tuta.io>
2022-12-11 17:26:48 +0530

    day 5

---
+++ 2022/5/p2.py
@@ -0,0 +1,48 @@
+from itertools import islice
+
+
+# generic parser to convert the stacks into python lists
+def get_stacks(lines):
+    length = int(lines[-1].strip()[-1])
+    stacks = dict()
+    for i in range(1, length+1):
+        stacks[i] = list()
+
+    for line in lines[:-1]:
+        it = iter(line)
+        n = 1
+        while crate := list(islice(it, 4)):
+            if crate[1] != ' ':
+                stacks[n].append(crate[1])
+            n += 1
+
+    for i in range(1, length+1):
+        stacks[i].reverse()
+
+    return stacks, length
+
+
+def main():
+    fp = open('input', 'r')
+    # get only the stacks portion of the input
+    lines = fp.readlines()
+    ni = lines.index('\n')
+    first, second = lines[:ni], lines[ni+1:]
+    stacks, length = get_stacks(first)
+
+    for ln in second:
+        lntokens = ln.split()
+        num, stack_from, stack_to = lntokens[1], lntokens[3], lntokens[5]
+        elems = []
+        for _ in range(int(num)):
+            elems.append(stacks[int(stack_from)].pop())
+        elems.reverse()
+        stacks[int(stack_to)].extend(elems)
+
+    for i in range(1, length+1):
+        print(stacks[i][-1], end='')
+    print()
+
+
+if __name__ == '__main__':
+    main()
