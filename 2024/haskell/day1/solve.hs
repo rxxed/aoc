8667af57005628f3eb705d4c026922ac93fe8ce6
Raaed Shaikh <raaed@tuta.io>
2024-12-03 15:38:43 +0530

    2024 day 1 improvement

--- 2024/haskell/day1/solve.hs
+++ 2024/haskell/day1/solve.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
 import Data.List
-import Language.Haskell.TH
 import System.IO.Unsafe
 
 input :: [String]
@@ -8,14 +6,10 @@ input = unsafePerformIO $ do
     contents <- readFile "input"
     return $ words contents
 
-fstCol :: [Int]
-fstCol = [read x | (x,i) <- zip input [0..], even i]
+cols :: ([Int], [Int])
+cols = both (map snd) $ partition (even . fst) $ zip [0..] (map read input)
+  where
+    both f (t1, t2) = (f t1, f t2)
 
-sndCol :: [Int]
-sndCol = [read x | (x,i) <- zip input [0..], odd i]
-
-solve1 :: Int
-solve1 = sum $ map (\(x,y) -> abs $ x - y) $ zip (sort fstCol) (sort sndCol)
-
-solve2 :: Int
-solve2 = sum [x * (length $ filter (== x) sndCol) | x <- fstCol]
+part1 = sum $ map (\(x,y) -> abs (x - y)) $ zip (sort $ fst cols) (sort $ snd cols)
+part2 = sum [x * ((length . filter (== x) . snd) cols) | x <- fst cols]
