3b7fa1dbc7c5be9ec2ee582dab518aa8e3a92461
Raaed Shaikh <raaed@tuta.io>
2024-12-03 15:44:50 +0530

    2024 day 2

--- 2024/haskell/day2/solve.hs
+++ 2024/haskell/day2/solve.hs
@@ -14,7 +14,7 @@ isSafe xs = allUnique xs && isSortedEitherWay xs && ((<4) . maximum . changes) (
     isSortedEitherWay :: Ord a => [a] -> Bool
     isSortedEitherWay xs = xs == sort xs || xs == reverse (sort xs)
     changes :: Num a => [a] -> [a]
-    changes xs = map abs $ zipWith (-) xs $ tail xs
+    changes xs = map abs $ zipWith (-) xs (tail xs)
 
 isSafeWithDampening :: (Num a, Ord a) => [a] -> Bool
 isSafeWithDampening xs = isSafe xs || any isSafe (sublists xs)
@@ -22,12 +22,6 @@ isSafeWithDampening xs = isSafe xs || any isSafe (sublists xs)
     sublists :: [a] -> [[a]]
     sublists xs = zipWith (++) (inits xs) (tail $ tails xs)
 
-solve1 :: Int
-solve1 = length $ filter isSafe levels
-  where
-    levels = map (map read) $ map words input
-
-solve2 :: Int
-solve2 = length $ filter isSafeWithDampening levels
-  where
-    levels = map (map read) $ map words input
+levels = map (map read) $ map words input
+part1 = length $ filter isSafe levels
+part2 = length $ filter isSafeWithDampening levels
